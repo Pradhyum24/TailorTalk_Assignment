c7cb075ab4359352ce6ea4f31d04f62e03a4a4ff
Pradhyum Sirupa <90834455+Pradhyum24@users.noreply.github.com>
2025-06-27 23:39:59 +0530

    Delete backend/test_calendar.py

--- backend/test_calendar.py
+++
@@ -1,8 +0,0 @@
-from calendar_utils import create_event
-import datetime
-
-start = datetime.datetime.now() + datetime.timedelta(days=1, hours=1)
-end = start + datetime.timedelta(minutes=30)
-
-link = create_event(start, end)
-print("📅 Event created:", link)
